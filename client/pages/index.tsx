ff8042be4c71f92b07ab6b5c35c47b4815d74700
s-alad <n7.saad@gmail.com>
2024-03-14 16:44:54 -0400

    nextjs search

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,6 +3,7 @@ import s from "./index.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import Web3 from 'web3';
+const { Conflux } = require('js-conflux-sdk');
 
 
 interface Entry {
@@ -20,6 +21,12 @@ export default function Index() {
   const [text, setText] = useState("")
   const [results, setResults] = useState<Entry[]>([])
 
+
+  const conflux = new Conflux({
+    url: 'https://evmtestnet.confluxrpc.com/',
+    logger: console,
+    networkId: 1,
+  });
   const contractAddress = '0x2689DF809bC5735334bb873e1dB1A143B2e639FA';
   const contractABI = [{
     "inputs": [],
@@ -240,13 +247,13 @@ export default function Index() {
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
-}];
+  }];
+  const contract = conflux.Contract({ abi: contractABI, address: contractAddress });
+
 
   //Need to add searching animation while its fetching from the blockchain
   async function search(searchTerm: string) {
-    const web3 = new Web3('https://evmtestnet.confluxrpc.com');
-    const contract = new web3.eth.Contract(contractABI, contractAddress);
-    let result;
+    /* let result;
 
     try {
       console.log('searching for:', searchTerm);
@@ -255,7 +262,7 @@ export default function Index() {
       console.error('Error:', error);
       throw error;
     }
-      setResults(result as Entry[]);
+    setResults(result as Entry[]); */
   }
 
   return (
@@ -286,11 +293,11 @@ export default function Index() {
               })
             }
           </div>
-          : 
+          :
           <div className={s.library}>
             <Image src="/Ancient.jpg" alt="Alexandria Library" width={500} height={500} />
             <p>
-            Alexandria is the world's first blockchain-based public library. At Alexandria, we believe that information should be free, easily accessible, and free from censorship. For much of the internet's history this has been the case however we've identified a flaw in the model used by platforms such as The Internet Archive and Google Scholar. These sites, despite their value, are costly to operate and maintain due to their reliance on centralized servers. While generous donations have sustained them thus far, future financial stability is uncertain. To address this, we've developed a new, more economical and sustainable method for storing digital content. Utilizing blockchain technology, specifically IPFS and an EVM-compatible chain, we can permanently archive digital information at a minimal cost. The only expense is maintaining an IPFS node, which, in our case, is a Raspberry Pi. Through this innovative approach, we aim to preserve and provide open access to a wealth of copyright-free books, news articles, research papers, and more, ensuring they remain free and accessible for everyone.
+              Alexandria is the world's first blockchain-based public library. At Alexandria, we believe that information should be free, easily accessible, and free from censorship. For much of the internet's history this has been the case however we've identified a flaw in the model used by platforms such as The Internet Archive and Google Scholar. These sites, despite their value, are costly to operate and maintain due to their reliance on centralized servers. While generous donations have sustained them thus far, future financial stability is uncertain. To address this, we've developed a new, more economical and sustainable method for storing digital content. Utilizing blockchain technology, specifically IPFS and an EVM-compatible chain, we can permanently archive digital information at a minimal cost. The only expense is maintaining an IPFS node, which, in our case, is a Raspberry Pi. Through this innovative approach, we aim to preserve and provide open access to a wealth of copyright-free books, news articles, research papers, and more, ensuring they remain free and accessible for everyone.
             </p>
           </div>
       }
